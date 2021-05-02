8b8eaecfa7328a36cac2be0cf5bb80edb6e18323
INDRASHIS PAUL <indrashis985@gmail.com>
2021-05-03 00:57:22 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,21 +2,17 @@ import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
-from keras.applications.vgg19 import preprocess_input
-from keras.applications.vgg19 import decode_predictions
 
-model = tf.keras.models.load_model('./models/vgg19.h5', compile=False)
+model = tf.keras.models.load_model('./models/my_model.h5', compile=False)
 
 def process_image(image):
     '''
-    Make an image ready-to-use by VGG19
+    Make an image ready-to-use by the model
     '''
     # convert the image pixels to a numpy array
     image = img_to_array(image)
     # reshape data for the model
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    # prepare the image for the VGG model
-    image = preprocess_input(image)
+    image = image.reshape((-1, 28, 28, 3))
 
     return image
 
@@ -39,7 +35,11 @@ def predict_class(image):
 if __name__ == '__main__':
     ''' for test'''
     # load an image from file
-    image = load_img('../image.jpg', target_size=(224, 224))
+    image = load_img('../image1.jpg', target_size=(224, 224))
     image = process_image(image)
     prediction, percentage = predict_class(image)
+    # write scores to a file
+    with open("metrics.txt", "w") as f:
+        f.write("Predicted Class: %2.2f%%\n" % prediction)
+        f.write("Test accuracy score: %2.2f%%\n" % test_score)
     print(prediction, percentage)
