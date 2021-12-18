f945fb82ea6ab7ba732fb80da55606cf754a63b3
IndraP24 <indrashis985@gmail.com>
2021-12-18 22:39:05 +0530

    Update frontend design

--- js/mobile-net.js
+++ js/mobile-net.js
@@ -1,106 +1,111 @@
 let model;
 async function loadModel() {
-	console.log("model loading..");
-	loader = document.getElementById("progress-box");
-	load_button = document.getElementById("load-button");
-	loader.style.display = "block";
-	modelName = "mobilenet";
-	model = undefined;
-	model = await tf.loadLayersModel('./sc_detector/artifacts/tfjs/my_model/model.json');
-	loader.style.display = "none";
-	load_button.disabled = true;
-	load_button.innerHTML = "Loaded Model";
-	console.log("model loaded..");
+  console.log("model loading..");
+  loader = document.getElementById("progress-box");
+  load_button = document.getElementById("load-button");
+  loader.style.display = "block";
+  modelName = "mobilenet";
+  model = undefined;
+  model = await tf.loadLayersModel(
+    "./sc_detector/artifacts/tfjs/my_model/model.json"
+  );
+  loader.style.display = "none";
+  load_button.disabled = true;
+  load_button.innerHTML = "Loaded Model";
+  console.log("model loaded..");
 }
 
 async function loadFile() {
-	console.log("image is in loadfile..");
-	document.getElementById("select-file-box").style.display = "table-cell";
-  	document.getElementById("predict-box").style.display = "table-cell";
-  	document.getElementById("prediction").innerHTML = "Click predict to find the type of Skin Cancer!";
-  	var fileInputElement = document.getElementById("select-file-image");
-  	console.log(fileInputElement.files[0]);
-    renderImage(fileInputElement.files[0]);
+  console.log("image is in loadfile..");
+  document.getElementById("select-file-box").style.display = "table-cell";
+  document.getElementById("predict-box").style.display = "table-cell";
+  document.getElementById("prediction").innerHTML =
+    "Click predict to find the type of Skin Cancer!";
+  var fileInputElement = document.getElementById("select-file-image");
+  console.log(fileInputElement.files[0]);
+  renderImage(fileInputElement.files[0]);
 }
 
 function renderImage(file) {
   var reader = new FileReader();
   console.log("image is here..");
-  reader.onload = function(event) {
+  reader.onload = function (event) {
     img_url = event.target.result;
     console.log("image is here2..");
     document.getElementById("test-image").src = img_url;
-  }
+  };
   reader.readAsDataURL(file);
 }
 
-
 async function predButton() {
-	console.log("model loading..");
+  console.log("model loading..");
 
-	if (model == undefined) {
-		alert("Please load the model first..")
-	}
-	if (document.getElementById("predict-box").style.display == "none") {
-		alert("Please load an image using 'Demo Image' or 'Upload Image' button..")
-	}
-	console.log(model);
-	let image  = document.getElementById("test-image");
-	let tensor = preprocessImage(image, modelName);
+  if (model == undefined) {
+    alert("Please load the model first..");
+  }
+  if (document.getElementById("predict-box").style.display == "none") {
+    alert("Please load an image using 'Demo Image' or 'Upload Image' button..");
+  }
+  console.log(model);
+  let image = document.getElementById("test-image");
+  let tensor = preprocessImage(image, modelName);
 
-	let predictions = await model.predict(tensor).data();
-	let results = Array.from(predictions)
-		.map(function (p, i) {
-			return {
-				probability: p,
-				className: TARGET_CLASSES[i]
-			};
-		}).sort(function (a, b) {
-			return b.probability - a.probability;
-		}).slice(0, 5);
+  let predictions = await model.predict(tensor).data();
+  let results_all = Array.from(predictions)
+    .map(function (p, i) {
+      return {
+        probability: p,
+        className: TARGET_CLASSES[i],
+      };
+    })
+    .sort(function (a, b) {
+      return b.probability - a.probability;
+    });
 
-	document.getElementById("predict-box").style.display = "block";
-	document.getElementById("prediction").innerHTML = "MobileNet prediction <br><b>" + results[0].className + "</b>";
+  let results = results_all.slice(0, 3);
 
-	var ul = document.getElementById("predict-list");
-	ul.innerHTML = "";
-	results.forEach(function (p) {
-		console.log(p.className + " " + p.probability.toFixed(6));
-		var li = document.createElement("LI");
-		li.innerHTML = p.className + " " + p.probability.toFixed(6);
-		ul.appendChild(li);
-	});
+  document.getElementById("predict-box").style.display = "block";
+  document.getElementById("prediction").innerHTML =
+    "MobileNet prediction <br><b>" + results[0].className + "</b>";
 
+  var ul = document.getElementById("predict-list");
+  ul.innerHTML = "";
+  results.forEach(function (p) {
+    console.log(p.className + " " + p.probability.toFixed(6));
+    var li = document.createElement("LI");
+    li.innerHTML = p.className + " " + p.probability.toFixed(6);
+    ul.appendChild(li);
+  });
 }
 
 function preprocessImage(image, modelName) {
-	let tensor = tf.browser.fromPixels(image)
-		.resizeNearestNeighbor([28, 28])
-		.toFloat();
+  let tensor = tf.browser
+    .fromPixels(image)
+    .resizeNearestNeighbor([28, 28])
+    .toFloat();
 
-	if (modelName === undefined) {
-		return tensor.expandDims();
-	} else if (modelName === "mobilenet") {
-		let offset = tf.scalar(127.5);
-		return tensor.sub(offset)
-			.div(offset)
-			.expandDims();
-	} else {
-		alert("Unknown model name..")
-	}
+  if (modelName === undefined) {
+    return tensor.expandDims();
+  } else if (modelName === "mobilenet") {
+    let offset = tf.scalar(127.5);
+    return tensor.sub(offset).div(offset).expandDims();
+  } else {
+    alert("Unknown model name..");
+  }
 }
 
 function loadDemoImage() {
-	document.getElementById("predict-box").style.display = "table-cell";
-  	document.getElementById("prediction").innerHTML = "Click predict to find the type of Skin Cancer!";
-	document.getElementById("select-file-box").style.display = "table-cell";
-	document.getElementById("predict-list").innerHTML = "";
+  document.getElementById("predict-box").style.display = "table-cell";
+  document.getElementById("prediction").innerHTML =
+    "Click predict to find the type of Skin Cancer!";
+  document.getElementById("select-file-box").style.display = "table-cell";
+  document.getElementById("predict-list").innerHTML = "";
 
-	base_path = "./images/samplepic.jpg"
-	// maximum = 4;
-	// minimum = 1;
-	// var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-	// img_path = base_path + randomnumber + ".jpeg"
-	img_path = base_path
-	document.getElementById("test-image").src = img_path;
+  base_path = "./images/samplepic.jpg";
+  // maximum = 4;
+  // minimum = 1;
+  // var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  // img_path = base_path + randomnumber + ".jpeg"
+  img_path = base_path;
+  document.getElementById("test-image").src = img_path;
 }
